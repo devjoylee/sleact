8ff8b1ed83bc16c2b9e8eff98a585cb5af664ff2
Joy Lee <joylee.dev@gmail.com>
2022-03-31 23:40:45 +0900

    💄 add global styles & Sign Up page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage, SignUpPage } from 'pages';
+import { Global } from '@emotion/react';
+import { reset } from 'styles/reset';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigate replace to='/login' />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignUpPage />} />
-    </Routes>
+    <>
+      <Global styles={reset} />
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/login' />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+    </>
   );
 };
 
--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,4 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Label, Input, LinkContainer, Button, Header } from './styles';
+
 export const SignUpPage = () => {
-  return <div>회원가입</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id='signup_page'>
+      <Header>Slack</Header>
+      <Form onSubmit={handleSubmit}>
+        <Label id='email-label'>
+          <span>이메일 주소</span>
+          <Input type='email' id='email' name='email' />
+        </Label>
+        <Label id='nickname-label'>
+          <span>닉네임</span>
+          <Input type='text' id='nickname' name='nickname' />
+        </Label>
+        <Label id='password-label'>
+          <span>비밀번호</span>
+          <Input type='password' id='password' name='password' />
+        </Label>
+        <Label id='password-check-label'>
+          <span>비밀번호 확인</span>
+          <Input type='password' id='password-check' name='password-check' />
+        </Label>
+        <Button type='submit'>회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <Link to='/login'>로그인 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
-export {};
